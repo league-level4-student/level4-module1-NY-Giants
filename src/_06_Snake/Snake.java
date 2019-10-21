c9edba442e3eca5624c76cc6876c5e66820571a1
NY-Giants <Giants13>
2019-10-20 17:22:59 -0700

    gg
    gg

--- src/_06_Snake/Snake.java
+++ src/_06_Snake/Snake.java
@@ -26,6 +26,7 @@ public class Snake {
 	public void feed() {
 		//1. add a new SnakeSegment object to the snake
 		snake.add(new SnakeSegment(snake.get(0).getLocation(), BODY_SIZE));
+		
 	}
 
 	public Location getHeadLocation() {
